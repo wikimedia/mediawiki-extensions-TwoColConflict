e9c588dfb4f270974154a5ab007d8e3e074185ee
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2016-11-22 15:01:30 +0100

    Extend browsertests to test new conflict-view
    Bug: T149720
    Change-Id: I86b4b3f397af22658da4577915f9c434ba011167

--- tests/browser/features/support/pages/edit_conflict_page.rb
+++ tests/browser/features/support/pages/edit_conflict_page.rb
@@ -6,4 +6,9 @@ class EditConflictPage
   div(:twocolconflict_changes_text, id: 'mw-twocolconflict-changes-editor')
   div(:twocolconflict_editor_desc, css: '.mw-twocolconflict-editor-col .mw-twocolconflict-col-desc')
   text_area(:twocolconflict_editor_text, css: '.mw-twocolconflict-editor-col textarea')
+  div(:twocolconflict_changes_same, css: '#mw-twocolconflict-changes-editor .mw-twocolconflict-diffchange-same')
+  div(:twocolconflict_changes_foreign, css: '#mw-twocolconflict-changes-editor .mw-twocolconflict-diffchange-foreign')
+  div(:twocolconflict_changes_own, css: '#mw-twocolconflict-changes-editor .mw-twocolconflict-diffchange-own')
+  div(:twocolconflict_changes_title_foreign, css: '#mw-twocolconflict-changes-editor .mw-twocolconflict-diffchange-foreign .mw-twocolconflict-diffchange-title')
+  div(:twocolconflict_changes_title_own, css: '#mw-twocolconflict-changes-editor .mw-twocolconflict-diffchange-own .mw-twocolconflict-diffchange-title')
 end
