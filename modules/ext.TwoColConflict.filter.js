c9cfb7ce11e19854fb1e85596d18e048a8b932d0
WMDE-Fisch <christoph.jauera@wikimedia.de>
2017-01-17 13:06:53 +0100

    Select text in diff view with Ctrl+A
    See: https://stackoverflow.com/questions/985272#987376
    
    Bug: T155490
    Change-Id: Ib786f3ba73d658198a1779a1cc30def61cc6f19b

--- modules/ext.TwoColConflict.filter.js
+++ modules/ext.TwoColConflict.filter.js
@@ -1,4 +1,20 @@
 ( function ( $ ) {
+	function selectText( element ) {
+		var range, selection;
+
+		if ( document.body.createTextRange ) {
+			range = document.body.createTextRange();
+			range.moveToElementText( element );
+			range.select();
+		} else if ( window.getSelection ) {
+			selection = window.getSelection();
+			range = document.createRange();
+			range.selectNodeContents( element );
+			selection.removeAllRanges();
+			selection.addRange( range );
+		}
+	}
+
 	$( function () {
 		$( '.mw-twocolconflict-filter-options div' ).css( 'display', 'table-cell' );
 
@@ -14,6 +30,13 @@
 
 		$( '.mw-twocolconflict-diffchange-same-collapsed' ).click( function () {
 			$( 'input[name="mw-twocolconflict-same"]' )[ 0 ].closest( 'label' ).click();
+		} );
+
+		$( '.mw-twocolconflict-changes-editor' ).keydown( function( e ) {
+			if ( e.ctrlKey && e.keyCode === 65 ) { // CTRL + A
+				e.preventDefault();
+				selectText( this );
+			}
 		} );
 	} );
 }( jQuery ) );
