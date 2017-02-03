665841805d153187c7930b841232f89a4e411f67
Andrew-WMDE <andrew.kostka@wikimedia.de>
2017-02-03 18:36:41 +0100

    Create help dialog for TwoColConflict
    This patch introduces a help dialog for the edit conflict
    view. The module was rewritten to be configurable and re-
    usable for other extensions.
    
    In further patches this could be improved to have the CSS
    also more flexible and independent.
    
    The CSS and JS in this patch were loaded apart from each
    other. Loading CSS with the module did not quite work.
    
    Bug: T156250
    Change-Id: I23d16b0d6f8babfbe9983d727f162111c780e12b

--- modules/ext.TwoColConflict.init.js
+++ modules/ext.TwoColConflict.init.js
@@ -1,5 +1,6 @@
 ( function ( mw, $ ) {
-	var autoScroll = new mw.libs.twoColConflict.AutoScroll();
+	var autoScroll = new mw.libs.twoColConflict.AutoScroll(),
+		helpDialog = mw.libs.twoColConflict.HelpDialog;
 
 	function selectText( element ) {
 		var range, selection;
@@ -17,6 +18,43 @@
 		}
 	}
 
+	function initHelpDialog() {
+		helpDialog.init( {
+			name: 'twoColConflict',
+			title: 'twoColConflict-tutorial',
+			size: 'medium',
+			prev: 'twoColConflict-previous-dialog',
+			next: 'twoColConflict-next-dialog',
+			close: 'twoColConflict-close-dialog',
+			slides: [
+				{
+					message: 'twoColConflict-help-dialog-slide1',
+					imageClass: 'mw-twocolconflict-help-dialog-slide-1',
+					imageMode: 'landscape'
+				},
+				{
+					message: 'twoColConflict-help-dialog-slide2',
+					imageClass: 'mw-twocolconflict-help-dialog-slide-2',
+					imageMode: 'landscape'
+				},
+				{
+					message: 'twoColConflict-help-dialog-slide3',
+					imageClass: 'mw-twocolconflict-help-dialog-slide-3',
+					imageMode: 'landscape'
+				},
+				{
+					message: 'twoColConflict-help-dialog-slide4',
+					imageClass: 'mw-twocolconflict-help-dialog-slide-4',
+					imageMode: 'landscape'
+				}
+			]
+		} );
+
+		$( 'button[name="mw-twocolconflict-show-help"]' ).click( function () {
+			helpDialog.show();
+		} );
+	}
+
 	$( function () {
 		$( '.mw-twocolconflict-changes-editor' ).keydown( function( e ) {
 			if ( e.ctrlKey && e.keyCode === 65 ) { // CTRL + A
@@ -31,5 +69,7 @@
 		$( window ).on( 'resize', function() {
 			autoScroll.setScrollBaseData();
 		} );
+
+		initHelpDialog();
 	} );
 }( mediaWiki, jQuery ) );
