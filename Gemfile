1bae2586cbee73e3c054650e1ca327e38a7a898e
WMDE-Fisch <christoph.jauera@wikimedia.de>
2017-02-28 13:01:15 +0100

    Update tests to Selenium 3
    The waitr click on the checkbox seems to be broken or is broken with
    intend since the actual OOUI checkbox has an opacity of 0.
    
    Also made use of PageObject method for checkboxes and the name selector.
    
    Bug: T158074
    Change-Id: I97e8e60b06d77c073090f9e76c835f2f6890f284

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'http://rubygems.org'
 
-gem 'mediawiki_selenium', '~> 1.7.0'
+gem 'mediawiki_selenium', '~> 1.8.0'
 gem 'require_all', '~> 1.3', '>= 1.3.3'
 gem 'rubocop', '~> 0.41.0', require: false
--- tests/browser/features/support/pages/special_preferences_page.rb
+++ tests/browser/features/support/pages/special_preferences_page.rb
@@ -2,21 +2,22 @@ class SpecialPreferencesPage
   include PageObject
   page_url 'Special:Preferences'
 
-  a(:beta_features_tab, css: '#preftab-betafeatures')
-  text_field(:twocolconflict_checkbox, css: '[name=wptwocolconflict]')
+  link(:beta_features_tab, css: '#preftab-betafeatures')
+  checkbox(:twocolconflict_checkbox, name: 'wptwocolconflict')
+  div(:twocolconflict_checkbox_div, xpath: '//*[@name="wptwocolconflict"]//parent::div')
   button(:submit_button, css: '#prefcontrol')
 
   def enable_twocolconflict
-    beta_features_tab_element.when_present.click
-    return unless twocolconflict_checkbox_element.attribute('checked').nil?
-    twocolconflict_checkbox_element.click
-    submit_button_element.when_present.click
+    beta_features_tab_element.when_visible.click
+    return if twocolconflict_checkbox_checked?
+    twocolconflict_checkbox_div_element.click
+    submit_button_element.when_visible.click
   end
 
   def disable_twocolconflict
-    beta_features_tab_element.when_present.click
-    return if twocolconflict_checkbox_element.attribute('checked').nil?
-    twocolconflict_checkbox_element.click
-    submit_button_element.when_present.click
+    beta_features_tab_element.when_visible.click
+    return unless twocolconflict_checkbox_checked?
+    twocolconflict_checkbox_div_element.click
+    submit_button_element.when_visible.click
   end
 end
