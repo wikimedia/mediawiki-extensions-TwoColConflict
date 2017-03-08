33b628d3980a177c14f7cc7c7c2a089e2fac8a0d
WMDE-Fisch <christoph.jauera@wikimedia.de>
2017-03-08 19:10:07 +0100

    Add basic browser tests for filter options
    Bug: T159522
    Change-Id: I44f71ad923421ed37d2e7d65ee80dda2628c45bc

--- tests/browser/features/support/pages/edit_conflict_page.rb
+++ tests/browser/features/support/pages/edit_conflict_page.rb
@@ -7,8 +7,28 @@ class EditConflictPage
   div(:twocolconflict_editor_desc, css: '.mw-twocolconflict-editor-col .mw-twocolconflict-col-desc')
   text_area(:twocolconflict_editor_text, css: '.mw-twocolconflict-editor-col textarea')
   div(:twocolconflict_changes_same, css: '#mw-twocolconflict-changes-editor .mw-twocolconflict-diffchange-same')
+  div(
+      :twocolconflict_changes_same_full,
+      css: '#mw-twocolconflict-changes-editor .mw-twocolconflict-diffchange-same-full'
+  )
+  div(
+      :twocolconflict_changes_same_collapsed,
+      css: '#mw-twocolconflict-changes-editor .mw-twocolconflict-diffchange-same-collapsed'
+  )
   div(:twocolconflict_changes_foreign, css: '#mw-twocolconflict-changes-editor .mw-twocolconflict-diffchange-foreign')
   div(:twocolconflict_changes_own, css: '#mw-twocolconflict-changes-editor .mw-twocolconflict-diffchange-own')
-  div(:twocolconflict_changes_title_foreign, css: '#mw-twocolconflict-changes-editor .mw-twocolconflict-diffchange-foreign .mw-twocolconflict-diffchange-title')
-  div(:twocolconflict_changes_title_own, css: '#mw-twocolconflict-changes-editor .mw-twocolconflict-diffchange-own .mw-twocolconflict-diffchange-title')
+  div(
+      :twocolconflict_changes_title_foreign,
+      css: '#mw-twocolconflict-changes-editor .mw-twocolconflict-diffchange-foreign .mw-twocolconflict-diffchange-title'
+  )
+  div(:twocolconflict_changes_title_own,
+      css: '#mw-twocolconflict-changes-editor .mw-twocolconflict-diffchange-own .mw-twocolconflict-diffchange-title'
+  )
+
+  div(:twocolconflict_option_both_div, xpath: '(//*[@name="mw-twocolconflict-show-changes"]//parent::div)[1]')
+  div(:twocolconflict_option_mine_div, xpath: '(//*[@name="mw-twocolconflict-show-changes"]//parent::div)[2]')
+
+  radio_button(:twocolconflict_option_show, xpath: '(//*[@name="mw-twocolconflict-same"])[1]')
+  div(:twocolconflict_option_show_div, xpath: '(//*[@name="mw-twocolconflict-same"]//parent::div)[1]')
+  div(:twocolconflict_option_hide_div, xpath: '(//*[@name="mw-twocolconflict-same"]//parent::div)[2]')
 end
