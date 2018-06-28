94436b2c57d9c98e07d91f2509cca11fd85bf7bd
Thiemo Kreuz <thiemo.kreuz@wikimedia.de>
2018-06-28 12:42:25 +0200

    Consistent 1-level indention in LineBasedUnifiedDiffFormatterTest
    Some of this used 2 levels of indention, while other elements (sometimes
    in the same array) used 1 level of indention only.
    
    Change-Id: I1dabe70c783a9360f0fdb9ef154744dd4c43c222

--- tests/phpunit/includes/InlineTwoColConflict/LineBasedUnifiedDiffFormatterTest.php
+++ tests/phpunit/includes/InlineTwoColConflict/LineBasedUnifiedDiffFormatterTest.php
@@ -40,82 +40,74 @@ class LineBasedUnifiedDiffFormatterTest extends MediaWikiTestCase {
 			[
 				'before' => 'Just text.',
 				'after' => 'Just text. And more.',
-				'result' =>
-					[
-						1 =>
-							[
-								[
-									'action' => 'delete',
-									'old' => 'Just text.',
-									'oldline' => 1
-								],
-								[
-									'action' => 'add',
-									'new' => 'Just text<ins class="diffchange">. And more</ins>.',
-									'newline' => 1
-								]
-							],
+				'result' => [
+					1 => [
+						[
+							'action' => 'delete',
+							'old' => 'Just text.',
+							'oldline' => 1
+						],
+						[
+							'action' => 'add',
+							'new' => 'Just text<ins class="diffchange">. And more</ins>.',
+							'newline' => 1
+						]
 					],
+				],
 			],
 			[
 				'before' => 'Just less text.',
 				'after' => 'Just less.',
-				'result' =>
-					[
-						1 =>
-							[
-								[
-									'action' => 'delete',
-									'old' => 'Just less <del class="diffchange">text</del>.',
-									'oldline' => 1
-								],
-								[
-									'action' => 'add',
-									'new' => 'Just less.',
-									'newline' => 1
-								]
-							]
-					],
+				'result' => [
+					1 => [
+						[
+							'action' => 'delete',
+							'old' => 'Just less <del class="diffchange">text</del>.',
+							'oldline' => 1
+						],
+						[
+							'action' => 'add',
+							'new' => 'Just less.',
+							'newline' => 1
+						]
+					]
+				],
 			],
 			[
-				'before' =>
-<<<TEXT
+				'before' => <<<TEXT
 Just multi-line text.
 Line number 2.
 TEXT
 				,
-				'after' =>
-<<<TEXT
+				'after' => <<<TEXT
 Just multi-line text.
 Line number 1.5.
 Line number 2.
 TEXT
 				,
-				'result' =>
-					[
-						1 => [
-							[
-								'action' => 'copy',
-								'copy' => 'Just multi-line text.',
-								'oldline' => 1,
-								'newline' => 1
-							]
+				'result' => [
+					1 => [
+						[
+							'action' => 'copy',
+							'copy' => 'Just multi-line text.',
+							'oldline' => 1,
+							'newline' => 1
 						],
-						2 =>
-							[
-								[
-									'action' => 'add',
-									'new' => '<ins class="diffchange">Line number 1.5.</ins>',
-									'newline' => 2
-								],
-								[
-									'action' => 'copy',
-									'copy' => 'Line number 2.',
-									'oldline' => 2,
-									'newline' => 3
-								]
-							]
 					],
+					2 => [
+						[
+							'action' => 'add',
+							'new' => '<ins class="diffchange">Line number 1.5.</ins>',
+							'newline' => 2
+						],
+						[
+							'action' => 'copy',
+							'copy' => 'Line number 2.',
+							'oldline' => 2,
+							'newline' => 3
+						]
+					]
+				],
 			],
 			[
 				'before' => <<<TEXT
@@ -146,140 +138,122 @@ TEXT
 				],
 			],
 			[
-				'before' =>
-<<<TEXT
+				'before' => <<<TEXT
 Just multi-line text.
 Line number 1.5.
 Line number 2.
 TEXT
 				,
-				'after' =>
-<<<TEXT
+				'after' => <<<TEXT
 Just multi-line text.
 Line number 1.5.
 TEXT
 				,
-				'result' =>
-					[
-						1 => [
-							[
-								'action' => 'copy',
-								'copy' => "Just multi-line text.\nLine number 1.5.",
-								'oldline' => 1,
-								'newline' => 1
-							]
+				'result' => [
+					1 => [
+						[
+							'action' => 'copy',
+							'copy' => "Just multi-line text.\nLine number 1.5.",
+							'oldline' => 1,
+							'newline' => 1
 						],
-						3 =>
-							[
-								[
-									'action' => 'delete',
-									'old' => '<del class="diffchange">Line number 2.</del>',
-									'oldline' => 3
-								]
-							]
 					],
+					3 => [
+						[
+							'action' => 'delete',
+							'old' => '<del class="diffchange">Line number 2.</del>',
+							'oldline' => 3
+						]
+					]
+				],
 			],
 			[
-				'before' =>
-<<<TEXT
+				'before' => <<<TEXT
 Just multi-line text.
 Line number 1.5.
 Line number 2.
 TEXT
 				,
-				'after' =>
-<<<TEXT
+				'after' => <<<TEXT
 Just multi-line test.
 Line number 2.
 Line number 3.
 TEXT
 				,
-				'result' =>
-					[
-						1 =>
-							[
-								[
-									'action' => 'delete',
-									'old' =>
-<<<TEXT
+				'result' => [
+					1 => [
+						[
+							'action' => 'delete',
+							'old' => <<<TEXT
 Just multi-line <del class="diffchange">text.</del>
 <del class="diffchange">Line number 1.5</del>.
 TEXT
-									,
-									'oldline' => 1
-								],
-								[
-									'action' => 'add',
-									'new' => 'Just multi-line <ins class="diffchange">test</ins>.',
-									'newline' => 1
-								]
-							],
-						3 => [
-							[
-								'action' => 'copy',
-								'copy' => 'Line number 2.',
-								'oldline' => 3,
-								'newline' => 2
-							]
+							,
+							'oldline' => 1
 						],
-						4 =>
-							[
-								[
-									'action' => 'add',
-									'new' => '<ins class="diffchange">Line number 3.</ins>',
-									'newline' => 3
-
-								]
-							],
+						[
+							'action' => 'add',
+							'new' => 'Just multi-line <ins class="diffchange">test</ins>.',
+							'newline' => 1
+						]
 					],
+					3 => [
+						[
+							'action' => 'copy',
+							'copy' => 'Line number 2.',
+							'oldline' => 3,
+							'newline' => 2
+						],
+					],
+					4 => [
+						[
+							'action' => 'add',
+							'new' => '<ins class="diffchange">Line number 3.</ins>',
+							'newline' => 3
+						]
+					],
+				],
 			],
 			[
-				'before' =>
-<<<TEXT
+				'before' => <<<TEXT
 Just multi-line text.
 To change number 2.
 To change number 3.
 TEXT
 				,
-				'after' =>
-<<<TEXT
+				'after' => <<<TEXT
 Just multi-line test.
 Line number 2 changed.
 Line number 3 also changed.
 TEXT
 				,
-				'result' =>
-					[
-						1 =>
-							[
-								[
-									'action' => 'delete',
-									'old' =>
-<<<TEXT
+				'result' => [
+					1 => [
+						[
+							'action' => 'delete',
+							'old' => <<<TEXT
 Just multi-line <del class="diffchange">text</del>.
 <del class="diffchange">To change </del>number 2.
 <del class="diffchange">To change </del>number 3.
 TEXT
-									,
-									'oldline' => 1
-								],
-								[
-									'action' => 'add',
-									'new' =>
-<<<TEXT
+							,
+							'oldline' => 1
+						],
+						[
+							'action' => 'add',
+							'new' => <<<TEXT
 Just multi-line <ins class="diffchange">test</ins>.
 <ins class="diffchange">Line </ins>number 2 <ins class="diffchange">changed</ins>.
 <ins class="diffchange">Line </ins>number 3 <ins class="diffchange">also changed</ins>.
 TEXT
-									,
-									'newline' => 1
-								]
-							],
+							,
+							'newline' => 1
+						]
 					],
+				],
 			],
 			[
-				'before' =>
-<<<TEXT
+				'before' => <<<TEXT
 Just a multi-line text.
 Line number two. This line is quite long!
 And that's line number three - even longer than the line before.
@@ -287,29 +261,26 @@ And that's line number three - even longer than the line before.
 Just another line with an empty line above.
 TEXT
 				,
-				'after' =>
-<<<TEXT
+				'after' => <<<TEXT
 Just a multi-line text.
 Add something new.
 Line number two. Now line number three and quite long!
 Add more new stuff.
 TEXT
 				,
-				'result' =>
-					[
-						1 => [
-							[
-								'action' => 'copy',
-								'copy' => 'Just a multi-line text.',
-								'oldline' => 1,
-								'newline' => 1
-							]
+				'result' => [
+					1 => [
+						[
+							'action' => 'copy',
+							'copy' => 'Just a multi-line text.',
+							'oldline' => 1,
+							'newline' => 1
 						],
-						2 =>
-							[
-								[
-									'action' => 'delete',
-									'old' =>
+					],
+					2 => [
+						[
+							'action' => 'delete',
+							'old' =>
 // @codingStandardsIgnoreStart
 <<<TEXT
 Line number two. <del class="diffchange">This </del>line <del class="diffchange">is </del>quite long!
@@ -318,12 +289,12 @@ Line number two. <del class="diffchange">This </del>line <del class="diffchange"
 <del class="diffchange">Just another line with an empty line above</del>.
 TEXT
 // @codingStandardsIgnoreEnd
-									,
-									'oldline' => 2
-								],
-								[
-									'action' => 'add',
-									'new' =>
+							,
+							'oldline' => 2
+						],
+						[
+							'action' => 'add',
+							'new' =>
 // @codingStandardsIgnoreStart
 <<<TEXT
 <ins class="diffchange">Add something new.</ins>
@@ -331,22 +302,20 @@ Line number two. <ins class="diffchange">Now </ins>line <ins class="diffchange">
 <ins class="diffchange">Add more new stuff</ins>.
 TEXT
 // @codingStandardsIgnoreEnd
-									,
-									'newline' => 2
-								]
-							],
+							,
+							'newline' => 2
+						]
 					],
+				],
 			],
 			[
-				'before' =>
-<<<TEXT
+				'before' => <<<TEXT
 Just a multi-line text.
 Line number two. This line is quite long!
 Line number three.
 TEXT
 				,
-				'after' =>
-<<<TEXT
+				'after' => <<<TEXT
 Just a multi-line text.
 Line number two. This line is now a bit longer!
 
@@ -355,46 +324,43 @@ And it gets even longer.
 Line number three.
 TEXT
 				,
-				'result' =>
-					[
-						1 => [
-							[
-								'action' => 'copy',
-								'copy' => 'Just a multi-line text.',
-								'oldline' => 1,
-								'newline' => 1
-							]
+				'result' => [
+					1 => [
+						[
+							'action' => 'copy',
+							'copy' => 'Just a multi-line text.',
+							'oldline' => 1,
+							'newline' => 1
 						],
-						2 =>
-							[
-								[
-									'action' => 'delete',
-									'old' => 'Line number two. This line is ' .
-										'<del class="diffchange">quite long</del>!',
-									'oldline' => 2
-								],
-								[
-									'action' => 'add',
-									'new' =>
-<<<TEXT
+					],
+					2 => [
+						[
+							'action' => 'delete',
+							'old' => 'Line number two. This line is ' .
+								'<del class="diffchange">quite long</del>!',
+							'oldline' => 2
+						],
+						[
+							'action' => 'add',
+							'new' => <<<TEXT
 Line number two. This line is <ins class="diffchange">now a bit longer</ins>!
 \u{00A0}
 <ins class="diffchange">And it gets even longer.</ins>
 \u{00A0}
 TEXT
-									,
-									'newline' => 2
-								],
-							],
-						3 => [
-							[
-								'action' => 'copy',
-								'copy' => 'Line number three.',
-								'oldline' => 3,
-								'newline' => 6
-							]
+							,
+							'newline' => 2
 						],
 					],
+					3 => [
+						[
+							'action' => 'copy',
+							'copy' => 'Line number three.',
+							'oldline' => 3,
+							'newline' => 6
+						]
+					],
+				],
 			],
 		];
 	}
