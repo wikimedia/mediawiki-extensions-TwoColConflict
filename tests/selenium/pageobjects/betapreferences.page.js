67dfda5590c97f41426ea2b556e64e3decb1f602
WMDE-Fisch <christoph.jauera@wikimedia.de>
2018-09-25 09:41:21 +0200

    Adjust browser tests to new preferences interface
    The new OOUI preferences interface made these changes necessary.
    
    Bug: T205381
    Change-Id: I029428088270325f53bc6d467bcd36eaba3da77a

--- tests/selenium/pageobjects/betapreferences.page.js
+++ tests/selenium/pageobjects/betapreferences.page.js
@@ -1,10 +1,10 @@
 const Page = require( 'wdio-mediawiki/Page' );
 
 class BetaPreferencesPage extends Page {
-	get betaPreferencesLink() { return browser.element( '#preftab-betafeatures' ); }
+	get betaPreferencesLink() { return browser.element( '#preferences .oo-ui-tabOptionWidget:nth-of-type(6)' ); }
 	get twoColCheckbox() { return browser.element( 'input[name=wptwocolconflict]' ); }
 	get twoColLabel() { return browser.element( '//*[@name="wptwocolconflict"]//parent::span' ); }
-	get submit() { return browser.element( '#prefcontrol' ); }
+	get submit() { return browser.element( '#prefcontrol button' ); }
 
 	open() {
 		super.openTitle( 'Special:Preferences' );
