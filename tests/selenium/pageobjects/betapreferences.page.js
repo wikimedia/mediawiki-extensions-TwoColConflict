e13c2f5f4b743639015b7885cd965aca07a47bcb
WMDE-Fisch <christoph.jauera@wikimedia.de>
2018-10-01 16:42:54 +0200

    Use fragment to open BetaFeatures preference page
    Change-Id: Id4b4543cd8a4ddb68a5bbded7d2445e478721f32
    Depends-On:  I4b6aa5e7e89e60f05e217fc557bf6786441855e6

--- tests/selenium/pageobjects/betapreferences.page.js
+++ tests/selenium/pageobjects/betapreferences.page.js
@@ -1,18 +1,17 @@
 const Page = require( 'wdio-mediawiki/Page' );
 
 class BetaPreferencesPage extends Page {
-	get betaPreferencesLink() { return browser.element( '#preferences .oo-ui-tabOptionWidget:nth-of-type(6)' ); }
 	get twoColCheckbox() { return browser.element( 'input[name=wptwocolconflict]' ); }
 	get twoColLabel() { return browser.element( '//*[@name="wptwocolconflict"]//parent::span' ); }
 	get submit() { return browser.element( '#prefcontrol button' ); }
 
 	open() {
-		super.openTitle( 'Special:Preferences' );
-		this.betaPreferencesLink.click();
+		super.openTitle( 'Special:Preferences', {}, 'mw-prefsection-betafeatures' );
 	}
 
 	enableTwoColConflictBetaFeature() {
 		this.open();
+		this.twoColLabel.waitForVisible();
 		if ( !this.twoColCheckbox.getAttribute( 'checked' ) ) {
 			this.twoColLabel.click();
 			this.submit.click();
