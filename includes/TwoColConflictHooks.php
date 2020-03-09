620b19db447491649a47ad10df9383de862beffa
Adam Wight <adam.wight@wikimedia.de>
2020-03-09 14:14:09 +0100

    Detect whether an edit came from VisualEditor
    I'm guessing blindly, but it seems that the `veswitched` parameter
    will always be set to true when an edit comes through VisualEditor.
    That gives us the ability to detect Javascript for any of the normal
    edit workflows.
    
    There's still a risk that we record false negatives due to gadgets,
    custom workflows, etc.
    
    Bug: T245722
    Change-Id: I517405b620283d4554970d4dbe1752642a29a6f8
    (cherry picked from commit c062d78bf60d6cad879e33f07d85b10a4ada5259)

--- includes/TwoColConflictHooks.php
+++ includes/TwoColConflictHooks.php
@@ -153,6 +153,7 @@ class TwoColConflictHooks {
 					'editTime' => $editPage->edittime,
 					'pageTitle' => $editPage->getTitle()->getText(),
 					'hasJavascript' => $request->getBool( 'mw-twocolconflict-js' )
+						|| $request->getBool( 'veswitched' ),
 				]
 			);
 		}
