8715b78a879551059bf3d7731cb9ba7571f27171
Thiemo Kreuz <thiemo.kreuz@wikimedia.de>
2020-03-24 10:25:14 +0100

    Heavily simplify mw.libs.twoColConflict.split.merger()
    This code should do the exact same thing, as far as I can tell. In
    detail:
    
    * The first two nested each() loops did something the find() method
    will do anyway. If the jQuery object contains more than one element,
    find will consider them all. No need to manually iterate the elements.
    
    * The check if a specific class name is present can as well be done
    as a CSS selectot in find(). Note there is no space between the two
    class names. This is critical.
    
    This is split from Ie79f69a. There is no functional change in this
    patch here.
    
    Bug: T244237
    Change-Id: I9904c73365403159e902af4b86a496a5c6768134

--- modules/SplitTwoColConflict/ext.TwoColConflict.Split.Merger.js
+++ modules/SplitTwoColConflict/ext.TwoColConflict.Split.Merger.js
@@ -20,30 +20,24 @@
 	function merger( $rows ) {
 		var textLines = [];
 
-		$rows.each( function ( $index, $row ) {
-			$( $row ).find( '.mw-twocolconflict-split-column' ).each( function ( $index, $column ) {
-				if (
-					// eslint-disable-next-line no-jquery/no-class-state
-					$( $column ).hasClass( 'mw-twocolconflict-split-copy' ) ||
-					// eslint-disable-next-line no-jquery/no-class-state
-					$( $column ).hasClass( 'mw-twocolconflict-split-selected' )
-				) {
-					var line = $( $column ).find( '.mw-twocolconflict-split-editor' )
-						.val().trim( '\r\n' );
+		$rows.find(
+			'.mw-twocolconflict-split-column.mw-twocolconflict-split-copy, ' +
+			'.mw-twocolconflict-split-column.mw-twocolconflict-split-selected'
+		).each( function ( index, element ) {
+			var line = $( element ).find( '.mw-twocolconflict-split-editor' )
+				.val().trim( '\r\n' );
 
-					if ( line !== '' ) {
-						var $extraLineFeeds = $( $column ).find(
-							'[name^="mw-twocolconflict-split-linefeeds"]'
-						);
+			if ( line !== '' ) {
+				var $extraLineFeeds = $( element ).find(
+					'[name^="mw-twocolconflict-split-linefeeds"]'
+				);
 
-						if ( $extraLineFeeds.length ) {
-							line += repeat( '\n', $extraLineFeeds.val() );
-						}
-
-						textLines.push( line );
-					}
+				if ( $extraLineFeeds.length ) {
+					line += repeat( '\n', $extraLineFeeds.val() );
 				}
-			} );
+
+				textLines.push( line );
+			}
 		} );
 
 		return textLines.join( '\n' );
