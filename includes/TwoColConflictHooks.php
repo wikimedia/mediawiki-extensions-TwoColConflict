23585fcde393868ba94636cf64e68185578a5ba4
Thiemo Kreuz <thiemo.kreuz@wikimedia.de>
2020-05-05 10:31:55 +0200

    Move EditPage::importFormData hook handler to separate file
    This particular hook handler grew quite big and contains a lot of code
    that should not be burried in a large, generic "hooks" class, in my
    opinion.
    
    Change-Id: I8ba04a233d6db3d3a6ee20f93ea36b5e5f7dd7d1

--- includes/TwoColConflictHooks.php
+++ includes/TwoColConflictHooks.php
@@ -11,10 +11,8 @@ use OutputPage;
 use TwoColConflict\SpecialConflictTestPage\TwoColConflictTestEditPage;
 use TwoColConflict\SplitTwoColConflict\ConflictFormValidator;
 use TwoColConflict\SplitTwoColConflict\ResolutionSuggester;
-use TwoColConflict\SplitTwoColConflict\SplitConflictMerger;
 use TwoColConflict\SplitTwoColConflict\SplitTwoColConflictHelper;
 use User;
-use WebRequest;
 
 /**
  * Hook handlers for the TwoColConflict extension.
@@ -67,67 +65,6 @@ class TwoColConflictHooks {
 			// WebRequest values before!
 			$request->setVal( 'editRevId', $request->getInt( 'parentRevId' ) );
 		}
-	}
-
-	/**
-	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/EditPage::importFormData
-	 *
-	 * @param EditPage $editPage
-	 * @param WebRequest $request
-	 */
-	public static function onImportFormData( EditPage $editPage, WebRequest $request ) {
-		$contentRows = $request->getArray( 'mw-twocolconflict-split-content' );
-		if ( $contentRows ) {
-			$extraLineFeeds = $request->getArray( 'mw-twocolconflict-split-linefeeds' ) ?? [];
-			$sideSelection = $request->getArray( 'mw-twocolconflict-side-selector' ) ?? [];
-
-			if ( $request->getBool( 'mw-twocolconflict-single-column-view' ) ) {
-				if ( !( new ConflictFormValidator() )->validateRequest( $request ) ) {
-					// When the request is invalid, drop any selection to force the original conflict to
-					// be re-created, and not silently resolved or corrupted.
-					$sideSelection = [];
-					foreach ( $contentRows as $num => &$row ) {
-						// Make sure the merger can't fall back to "other", but allow other fallbacks
-						if ( is_array( $row ) && key( $row ) === 'other' ) {
-							unset( $row['other'] );
-							if ( !$row ) {
-								unset( $contentRows[$num] );
-							}
-						}
-					}
-				} elseif ( $request->getVal( 'mw-twocolconflict-reorder' ) === 'reverse' ) {
-					[ $contentRows, $extraLineFeeds ] = self::swapTalkComments( $contentRows, $extraLineFeeds );
-				}
-			}
-
-			$editPage->textbox1 = SplitConflictMerger::mergeSplitConflictResults(
-				$contentRows,
-				$extraLineFeeds,
-				$sideSelection
-			);
-		}
-
-		if ( $request->getBool( 'mw-twocolconflict-disable-core-hint' ) ) {
-			$user = $editPage->getContext()->getUser();
-			if ( !$user->isAnon() ) {
-				$user->setOption( TwoColConflictContext::HIDE_CORE_HINT_PREFERENCE, '1' );
-				$user->saveSettings();
-			}
-		}
-	}
-
-	private static function swapTalkComments( array $contentRows, array $extraLineFeeds ) {
-		for ( $i = 0; $i < count( $contentRows ) - 1; $i++ ) {
-			if ( isset( $contentRows[$i]['other'] ) && isset( $contentRows[$i + 1]['your'] ) ) {
-				[ $contentRows[$i], $contentRows[$i + 1] ] =
-					[ $contentRows[$i + 1], $contentRows[$i] ];
-				[ $extraLineFeeds[$i], $extraLineFeeds[$i + 1] ] =
-					[ $extraLineFeeds[$i + 1] ?? 0, $extraLineFeeds[$i] ?? 0 ];
-				$i++;
-			}
-		}
-
-		return [ $contentRows, $extraLineFeeds ];
 	}
 
 	/**
---
+++ tests/phpunit/unit/EditPageImportFormDataHookHandlerTest.php
@@ -0,0 +1,240 @@
+<?php
+
+namespace TwoColConflict\Tests;
+
+use EditPage;
+use TwoColConflict\EditPageImportFormDataHookHandler;
+use WebRequest;
+use Wikimedia\TestingAccessWrapper;
+
+/**
+ * @covers \TwoColConflict\EditPageImportFormDataHookHandler
+ *
+ * @license GPL-2.0-or-later
+ */
+class EditPageImportFormDataHookHandlerTest extends \MediaWikiUnitTestCase {
+
+	public function provideWebRequests() {
+		return [
+			'empty request' => [ [], '' ],
+			'no content' => [ [ 'mw-twocolconflict-split-content' => [], ], '' ],
+			'minimal non-empty request' => [
+				[
+					'mw-twocolconflict-split-content' => [ [ 'copy' => 'a' ] ],
+				],
+				'a'
+			],
+			'bad non-array elements' => [
+				[
+					'mw-twocolconflict-split-content' => [ 'bad' ],
+					'mw-twocolconflict-split-linefeeds' => [ 1 ],
+				],
+				"bad\n"
+			],
+			'valid single column request' => [
+				[
+					'mw-twocolconflict-single-column-view' => true,
+					'mw-twocolconflict-split-content' => [ [ 'other' => 'a' ] ],
+				],
+				'a'
+			],
+			'single column request with invalid content' => [
+				[
+					'mw-twocolconflict-single-column-view' => true,
+					'mw-twocolconflict-split-content' => [
+						[ 'other' => 'a' ],
+						[ 'your' => 'b' ],
+						[ 'other' => 'c', 'bad key' => 'bad value' ],
+						'bad string',
+					],
+				],
+				"b\nbad value\nbad string"
+			],
+			'single column request with invalid side selection' => [
+				[
+					'mw-twocolconflict-single-column-view' => true,
+					'mw-twocolconflict-split-content' => [
+						[ 'other' => 'a' ],
+						[ 'your' => 'b' ],
+					],
+					'mw-twocolconflict-side-selector' => [ 'bad' ],
+				],
+				'b'
+			],
+
+			'trivial copy situation' => [
+				[
+					'mw-twocolconflict-side-selector' => [],
+					'mw-twocolconflict-split-content' => [
+						1 => [ 'copy' => 'abc' ],
+					],
+					'mw-twocolconflict-split-linefeeds' => [
+						1 => [ 'copy' => 0 ],
+					],
+				],
+				'abc',
+			],
+			'trailing linefeeds in the content are ignored' => [
+				[
+					'mw-twocolconflict-side-selector' => [
+						1 => 'other',
+					],
+					'mw-twocolconflict-split-content' => [
+						1 => [ 'other' => "abc\n", 'your' => 'def' ],
+					],
+					'mw-twocolconflict-split-linefeeds' => [
+						1 => [ 'other' => 0 ],
+					],
+				],
+				'abc',
+			],
+			'original trailing linefeed is restored' => [
+				[
+					'mw-twocolconflict-side-selector' => [
+						1 => 'other',
+					],
+					'mw-twocolconflict-split-content' => [
+						1 => [ 'other' => "abc\n\n", 'your' => 'def' ],
+					],
+					'mw-twocolconflict-split-linefeeds' => [
+						1 => [ 'other' => 1 ],
+					],
+				],
+				"abc\n",
+			],
+			'all possibilities in one request' => [
+				[
+					'mw-twocolconflict-side-selector' => [
+						2 => 'other',
+						4 => 'your',
+					],
+					'mw-twocolconflict-split-content' => [
+						1 => [ 'copy' => 'a' ],
+						2 => [ 'other' => 'b other', 'your' => 'b your' ],
+						3 => [ 'copy' => 'c' ],
+						4 => [ 'other' => 'd other', 'your' => 'd your' ],
+					],
+					'mw-twocolconflict-split-linefeeds' => [
+						4 => [ 'your' => 0 ],
+					],
+				],
+				"a\nb other\nc\nd your",
+			],
+		];
+	}
+
+	/**
+	 * @dataProvider provideWebRequests
+	 */
+	public function testOnEditPageImportFormData( array $requestData, string $expectedWikitext ) {
+		$editPage = $this->createMock( EditPage::class );
+		$request = $this->createRequest( $requestData );
+		EditPageImportFormDataHookHandler::onEditPageImportFormData( $editPage, $request );
+		$this->assertSame( $expectedWikitext, $editPage->textbox1 );
+	}
+
+	/**
+	 * @param array $requestParams
+	 *
+	 * @return WebRequest
+	 */
+	private function createRequest( array $requestParams ) {
+		$request = $this->createMock( WebRequest::class );
+		$getter = function ( string $name, $default ) use ( $requestParams ) {
+			return $requestParams[$name] ?? $default;
+		};
+		$request->method( 'getArray' )->willReturnCallback( $getter );
+		$request->method( 'getBool' )->willReturnCallback( $getter );
+		return $request;
+	}
+
+	public function provideTalkPageRequests() {
+		return [
+			'empty' => [
+				'contentRows' => [],
+				'extraLineFeeds' => [],
+				'expected' => [ [], [] ]
+			],
+			'other first' => [
+				'contentRows' => [
+					[ 'other' => '' ],
+					[ 'your' => '' ],
+				],
+				'extraLineFeeds' => [ 1, 2 ],
+				'expected' => [
+					[
+						[ 'your' => '' ],
+						[ 'other' => '' ],
+					],
+					[ 2, 1 ]
+				]
+			],
+			'leave copy rows untouched' => [
+				'contentRows' => [
+					[ 'copy' => 'a' ],
+					[ 'copy' => 'b' ],
+					[ 'other' => '' ],
+					[ 'your' => '' ],
+					[ 'copy' => 'c' ],
+				],
+				'extraLineFeeds' => [ 1, 2, 3, 4, 5 ],
+				'expected' => [
+					[
+						[ 'copy' => 'a' ],
+						[ 'copy' => 'b' ],
+						[ 'your' => '' ],
+						[ 'other' => '' ],
+						[ 'copy' => 'c' ],
+					],
+					[ 1, 2, 4, 3, 5 ]
+				]
+			],
+			'multiple pairs to swap' => [
+				'contentRows' => [
+					[ 'other' => 'a' ],
+					[ 'your' => 'b' ],
+					[ 'other' => 'c' ],
+					[ 'your' => 'd' ],
+				],
+				'extraLineFeeds' => [],
+				'expected' => [
+					[
+						[ 'your' => 'b' ],
+						[ 'other' => 'a' ],
+						[ 'your' => 'd' ],
+						[ 'other' => 'c' ],
+					],
+					[ 0, 0, 0, 0 ]
+				]
+			],
+			'do not swap back if my row is alreday before' => [
+				'contentRows' => [
+					[ 'your' => '' ],
+					[ 'other' => '' ],
+				],
+				'extraLineFeeds' => [],
+				'expected' => [
+					[
+						[ 'your' => '' ],
+						[ 'other' => '' ],
+					],
+					[]
+				]
+			],
+		];
+	}
+
+	/**
+	 * @dataProvider provideTalkPageRequests
+	 */
+	public function testSwapTalkComments(
+		array $contentRows,
+		array $extraLineFeeds,
+		array $expected
+	) {
+		/** @var EditPageImportFormDataHookHandler $hooks */
+		$hooks = TestingAccessWrapper::newFromClass( EditPageImportFormDataHookHandler::class );
+		$this->assertSame( $expected, $hooks->swapTalkComments( $contentRows, $extraLineFeeds ) );
+	}
+
+}
