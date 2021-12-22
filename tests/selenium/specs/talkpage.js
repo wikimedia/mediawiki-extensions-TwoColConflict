51c3fe2c32fc585bcb85d11f626ad6dbea726ccd
WMDE-Fisch <christoph.jauera@wikimedia.de>
2021-12-22 17:16:03 +0100

    [selenium] Merge some talk page browser tests
    Setting up the conflict is very expensive. Testing some of these
    things independent form each other does not gain much.
    
    Note that the order how some tests are executed now partly depends
    on the outcome of the test before. This is simillar to what was
    done in the `tourdialog` spec. It seems a good compromise between
    keeping things kind of apart and avoid setup costs.
    
    Bug: T282935
    Change-Id: I56e6c9344d76208ac6f353e6e845ef4d4fc3aeff

--- tests/selenium/specs/talkpage.js
+++ tests/selenium/specs/talkpage.js
@@ -11,21 +11,54 @@ describe( 'TwoColConflict', function () {
 		EditConflictPage.prepareEditConflict();
 	} );
 
-	it( 'shows the talk page screen correctly', function () {
-		TalkConflictPage.createTalkPageConflict();
+	describe( 'on talk page conflicts', function () {
+		before( function () {
+			TalkConflictPage.createTalkPageConflict();
+			EditConflictPage.waitForJS();
+		} );
 
-		assert( !TalkConflictPage.splitColumn.isExisting() );
+		it( 'shows the talk page screen correctly', function () {
+			assert( !TalkConflictPage.splitColumn.isExisting() );
 
-		assert( EditConflictPage.getParagraph( 'other' ) );
-		assert( EditConflictPage.getParagraph( 'your' ) );
-		assert( EditConflictPage.getParagraph( 'copy' ) );
+			assert( EditConflictPage.getParagraph( 'other' ) );
+			assert( EditConflictPage.getParagraph( 'your' ) );
+			assert( EditConflictPage.getParagraph( 'copy' ) );
 
-		// Only "your" block is editable
-		assert( EditConflictPage.getEditButton( 'your' ).isExisting() );
-		assert( !EditConflictPage.getEditButton( 'other' ).isExisting() );
-		assert( !EditConflictPage.getEditButton( 'copy' ).isExisting() );
+			// Only "your" block is editable
+			assert( EditConflictPage.getEditButton( 'your' ).isExisting() );
+			assert( !EditConflictPage.getEditButton( 'other' ).isExisting() );
+			assert( !EditConflictPage.getEditButton( 'copy' ).isExisting() );
 
-		assert( TalkConflictPage.isOtherBlockFirst() );
+			assert( TalkConflictPage.isOtherBlockFirst() );
+		} );
+
+		it( 'swaps blocks when switch button is clicked', function () {
+			TalkConflictPage.swapButton.click();
+
+			assert( TalkConflictPage.isYourBlockFirst() );
+		} );
+
+		it( 'shows correct preview when swapped', function () {
+			EditConflictPage.previewButton.click();
+
+			assert( EditConflictPage.previewView.waitForDisplayed() );
+
+			assert.strictEqual(
+				EditConflictPage.previewText.getText(),
+				'Line1 Line2 Line3 Comment B Comment A'
+			);
+		} );
+
+		it( 'stores correct merge when swapped and edited', function () {
+			TalkConflictPage.editMyComment( 'Comment B edited' );
+
+			EditConflictPage.submitButton.click();
+
+			assert.strictEqual(
+				FinishedConflictPage.pageWikitext,
+				'Line1\nLine2\nLine3\nComment B edited\nComment <span lang="de">A</span>'
+			);
+		} );
 	} );
 
 	it( 'shows the talk page screen on conflicts that also add new lines', function () {
@@ -38,75 +71,6 @@ describe( 'TwoColConflict', function () {
 		TalkConflictPage.talkRow.waitForDisplayed();
 
 		assert( !TalkConflictPage.splitColumn.isExisting() );
-	} );
-
-	it( 'swaps blocks when switch button is clicked', function () {
-		TalkConflictPage.createTalkPageConflict();
-		EditConflictPage.waitForJS();
-
-		TalkConflictPage.swapButton.click();
-
-		assert( TalkConflictPage.isYourBlockFirst() );
-	} );
-
-	it( 'shows correct preview after edit', function () {
-		TalkConflictPage.createTalkPageConflict();
-		EditConflictPage.getEditButton( 'your' ).waitForEnabled();
-
-		EditConflictPage.waitForJS();
-		TalkConflictPage.editMyComment( 'Comment edited' );
-		EditConflictPage.previewButton.click();
-
-		assert( EditConflictPage.previewView.waitForDisplayed() );
-
-		assert.strictEqual(
-			EditConflictPage.previewText.getText(),
-			'Line1 Line2 Line3 Comment A Comment edited'
-		);
-	} );
-
-	it( 'shows correct preview swapped', function () {
-		TalkConflictPage.createTalkPageConflict();
-		EditConflictPage.waitForJS();
-
-		TalkConflictPage.swapButton.click();
-
-		EditConflictPage.previewButton.click();
-
-		assert( EditConflictPage.previewView.waitForDisplayed() );
-
-		assert.strictEqual(
-			EditConflictPage.previewText.getText(),
-			'Line1 Line2 Line3 Comment B Comment A'
-		);
-	} );
-
-	it( 'stores correct merge after edit', function () {
-		TalkConflictPage.createTalkPageConflict();
-		EditConflictPage.getEditButton( 'your' ).waitForEnabled();
-
-		EditConflictPage.waitForJS();
-		TalkConflictPage.editMyComment( 'Comment edited' );
-		EditConflictPage.submitButton.click();
-
-		assert.strictEqual(
-			FinishedConflictPage.pageWikitext,
-			'Line1\nLine2\nLine3\nComment <span lang="de">A</span>\nComment edited'
-		);
-	} );
-
-	it( 'stores correct merge swapped', function () {
-		TalkConflictPage.createTalkPageConflict();
-		EditConflictPage.waitForJS();
-
-		TalkConflictPage.swapButton.click();
-
-		EditConflictPage.submitButton.click();
-
-		assert.strictEqual(
-			FinishedConflictPage.pageWikitext,
-			'Line1\nLine2\nLine3\nComment <span lang="en">B</span>\nComment <span lang="de">A</span>'
-		);
 	} );
 
 	// TODO: test for double-conflict, all text should be restored even if edited.
