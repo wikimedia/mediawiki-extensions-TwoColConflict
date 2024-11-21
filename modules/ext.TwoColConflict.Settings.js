d69a182558c2de01427616be732c62353e4c2844
thiemowmde <thiemo.kreuz@wikimedia.de>
2024-11-21 09:45:11 +0100

    Minimize unused settings code
    Change-Id: Id6fbfa75cf67f22556a20143c016c24404f63a4c

--- modules/ext.TwoColConflict.Settings.js
+++ modules/ext.TwoColConflict.Settings.js
@@ -6,41 +6,14 @@ const Settings = function () {};
 Object.assign( Settings.prototype, {
 	/**
 	 * @param {string} name
-	 * @param {string} defaultValue
-	 * @return {string|boolean}
-	 */
-	loadSetting: function ( name, defaultValue ) {
-		let setting;
-		if ( mw.user.isNamed() ) {
-			setting = mw.user.options.get( 'userjs-twocolconflict-' + name );
-		} else {
-			setting = mw.storage.get( 'mw-twocolconflict-' + name ) ||
-				mw.cookie.get( '-twocolconflict-' + name );
-		}
-
-		return setting !== null && setting !== false ? setting : defaultValue;
-	},
-
-	/**
-	 * @param {string} name
-	 * @param {boolean} defaultValue
 	 * @return {boolean}
 	 */
-	loadBoolean: function ( name, defaultValue ) {
-		return this.loadSetting( name, defaultValue ? '1' : '0' ) === '1';
-	},
-
-	/**
-	 * @param {string} name
-	 * @param {string} value
-	 */
-	saveSetting: function ( name, value ) {
+	loadBoolean: function ( name ) {
 		if ( mw.user.isNamed() ) {
-			( new mw.Api() ).saveOption( 'userjs-twocolconflict-' + name, value );
+			return mw.user.options.get( 'userjs-twocolconflict-' + name ) === '1';
 		} else {
-			if ( !mw.storage.set( 'mw-twocolconflict-' + name, value ) ) {
-				mw.cookie.set( '-twocolconflict-' + name, value ); // use cookie when localStorage is not available
-			}
+			return ( mw.storage.get( 'mw-twocolconflict-' + name ) ||
+				mw.cookie.get( '-twocolconflict-' + name ) ) === '1';
 		}
 	},
 
@@ -49,7 +22,14 @@ Object.assign( Settings.prototype, {
 	 * @param {boolean} value
 	 */
 	saveBoolean: function ( name, value ) {
-		this.saveSetting( name, value ? '1' : '0' );
+		value = value ? '1' : '0';
+		if ( mw.user.isNamed() ) {
+			( new mw.Api() ).saveOption( 'userjs-twocolconflict-' + name, value );
+		} else {
+			if ( !mw.storage.set( 'mw-twocolconflict-' + name, value ) ) {
+				mw.cookie.set( '-twocolconflict-' + name, value ); // use cookie when localStorage is not available
+			}
+		}
 	}
 } );
 
